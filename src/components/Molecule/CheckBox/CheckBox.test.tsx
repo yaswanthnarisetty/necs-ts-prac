325bf2601466587dfe12afc94db3aa02a9b510a9
Avinash Koduri <saiavinashkoduri@gmail.com>
2024-02-16 13:35:47 +0530

    changes

--- src/components/Molecule/CheckBox/CheckBox.test.tsx
+++ src/components/Molecule/CheckBox/CheckBox.test.tsx
@@ -4,7 +4,6 @@ import CheckBoxInput from ".";
 
 describe("Testing Checkbox",()=>{
     test("Testing CheckBox", () => {
-        // Render the CheckBoxInput component
         const { getByLabelText } = render(
           <CheckBoxInput name="testing" possible={true} label="Testing" />
         );  
@@ -12,7 +11,6 @@ describe("Testing Checkbox",()=>{
         expect(textElement).toBeInTheDocument();   
       });
       test("Testing Enabled CheckBox", () => {
-        // Render the CheckBoxInput component
         const { getByLabelText } = render(
           <CheckBoxInput name="testing" possible={true} label="Testing" />
         );  
@@ -20,35 +18,34 @@ describe("Testing Checkbox",()=>{
         expect(checkbox).toBeEnabled();
       });
       test("Testing Disabled CheckBox", () => {
-        // Render the CheckBoxInput component
         const { getByLabelText } = render(
           <CheckBoxInput name="testing" possible={false} label="Testing" />
         );  
         const checkbox = screen.getByRole("checkbox");
         expect(checkbox).toBeDisabled();
+    });
 
+
+        test("Clicking CheckBox toggles its false state", () => {
+            const { getByLabelText } = render(
+              <CheckBoxInput name="testing" possible={true} label="Testing" />
+            );
+            const checkbox = getByLabelText("Testing") as HTMLInputElement;
+            expect(checkbox.checked).toBe(false);
+            fireEvent.click(checkbox);
+            expect(checkbox.checked).toBe(true);
+            fireEvent.click(checkbox);
+            expect(checkbox.checked).toBe(false);
+          });
+        
+          test("Clicking CheckBox toggles its true state", () => {
+            const { getByLabelText } = render(
+              <CheckBoxInput name="testing" possible={true} label="Testing" />
+            );
+            const checkbox = getByLabelText("Testing") as HTMLInputElement;
+            fireEvent.click(checkbox);
+            expect(checkbox.checked).toBe(true);
+            fireEvent.click(checkbox);
+            expect(checkbox.checked).toBe(false);
+          });
 });
-
-test("Clicking CheckBox toggles its false state", () => {
-    const { getByLabelText } = render(
-      <CheckBoxInput name="testing" possible={true} label="Testing" />
-    );
-    const checkbox = getByLabelText("Testing") as HTMLInputElement;
-    expect(checkbox.checked).toBe(false);
-    fireEvent.click(checkbox);
-    expect(checkbox.checked).toBe(true);
-    fireEvent.click(checkbox);
-    expect(checkbox.checked).toBe(false);
-  });
-
-  test("Clicking CheckBox toggles its true state", () => {
-    const { getByLabelText } = render(
-      <CheckBoxInput name="testing" possible={true} label="Testing" />
-    );
-    const checkbox = getByLabelText("Testing") as HTMLInputElement;
-    fireEvent.click(checkbox);
-    expect(checkbox.checked).toBe(true);
-    fireEvent.click(checkbox);
-    expect(checkbox.checked).toBe(false);
-  });
-  });
