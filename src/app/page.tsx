a924ab3d80bc9e50a48afde181aec9b917a5b40b
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-02-16 17:39:14 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import InputButton from "@/components/Molecule/InputButton";
 import SideBar from "@/components/Molecule/SideBar";
 import SideBarItem from "@/components/Molecule/SideBarItem";
 import { ClipboardCheck, Star } from 'lucide-react';
-import { ReactElement } from "react";
+import { FormEvent, ReactElement, useState } from "react";
 
 export default function Home() {
   // const users = await trpc.Query.hello.query({ name: "R" });
@@ -22,15 +22,37 @@ export default function Home() {
   const sideBarItems: sideBarItemProp[] = [
     {
       name: "All",
-      icon: <ClipboardCheck size={16} color="#fff" />,
+      icon: <ClipboardCheck size={16} color="#000" />,
       active: true
     },
     {
       name: "Starred",
-      icon: <Star fill="#fff" size={16} color="#fff" />,
-      active: true
+      icon: <Star fill="#000" size={16} color="#000" />,
+      active: false
     }
   ]
+
+
+
+  const [todo, setTodo] = useState<string>("");
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    try {
+      const addTodoResponse = await fetch("url", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ todo }),
+        cache: "no-store"
+      })
+    } catch (error) {
+
+    }
+
+  }
 
   return (
 
@@ -40,13 +62,17 @@ export default function Home() {
         {/* <CheckBoxInput label="testing" possible={true} />
         <InputButton />
         {sideBarItems.map((item: sideBarItemProp) => <SideBarItem active={item.active} itemName={item.name} itemIcon={item.icon} />)} */}
-        <Box style={{ display: "grid", gridTemplateColumns: "30% 70%", gap: "20px", margin: "10px 100px" }}>
+        <Box style={{ display: "grid", gridTemplateColumns: "30% 70%", gap: "30px", margin: "10px 5%" }}>
 
-          <SideBar title={"Filters"}  height={"300px"} >
-            
+          <SideBar title={"Filters"} height={"300px"}>
+            <Box display="flex" justifyContent="flex-start" flexDirection="column" gap="15px" >
+              {sideBarItems.map((item: sideBarItemProp) => <SideBarItem active={item.active} itemName={item.name} itemIcon={item.icon} />)}
+            </Box>
           </SideBar>
-          <SideBar title={"Filters"}  height={"300px"}>
-            
+          <SideBar title={"Tasks"} height={"300px"}>
+            <Box width="100%">
+              <InputButton todo={todo} setTodo={setTodo} handleSubmit={handleSubmit} />
+            </Box>
           </SideBar>
         </Box>
 
