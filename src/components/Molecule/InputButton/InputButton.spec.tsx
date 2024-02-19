1a0f53e935591fc4ea75ce8901ff7ef0e2d12dc0
Ramesh <ramesh.gorekanti@gmail.com>
2024-02-19 11:04:08 +0530

    changes in input and text test cases

---
+++ src/components/Molecule/InputButton/InputButton.spec.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import InputButton from "./index";
+
+describe("InputButton Component", () => {
+  test("renders without errors", () => {
+    render(<InputButton todo="" setTodo={() => {}} handleSubmit={() => {}} />);
+    expect(screen.getByTestId("input-button-form")).toBeInTheDocument();
+  });
+
+  test("renders input field with default value", () => {
+    const defaultValue = "Default Value";
+    render(<InputButton todo={defaultValue} setTodo={() => {}} handleSubmit={() => {}} />);
+    const inputElement = screen.getByDisplayValue(defaultValue);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test("calls setTodo function on input change", () => {
+    const setTodoMock = jest.fn();
+    render(<InputButton todo="" setTodo={setTodoMock} handleSubmit={() => {}} />);
+
+    const inputValue = "New Todo";
+    const inputElement = screen.getByTestId("custom-input");
+    fireEvent.change(inputElement, { target: { value: inputValue } });
+
+    expect(setTodoMock).toHaveBeenCalledWith(inputValue);
+  });
+
+  test("calls handleSubmit function on form submission", () => {
+    const handleSubmitMock = jest.fn();
+    render(<InputButton todo="" setTodo={() => {}} handleSubmit={handleSubmitMock} />);
+
+    const formElement = screen.getByTestId("input-button-form");
+    fireEvent.submit(formElement);
+
+    expect(handleSubmitMock).toHaveBeenCalled();
+  });
+});
